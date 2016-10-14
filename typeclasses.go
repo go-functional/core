d00819af9f04a3cf4e9489610c59506fc75c7916
Aaron Schlesinger <aaron@deis.com>
2016-10-14 16:14:09 -0700

    adding some usage docs

--- typeclasses.go
+++ typeclasses.go
@@ -2,6 +2,24 @@ package funcgo
 
 // see http://learnyouahaskell.com/types-and-typeclasses
 
+// Each of the typeclasses herein are "adapters" for existing types. Other languages (Haskell, Scala) automatically convert existing types for use with these adapters, but in Go we have to do things more manually. Here's how:
+//
+// 1. create an implementation of one of the type classes
+//
+//	type intEq int
+//
+//	func (i intEq) Eq(e Eq) bool {
+//		i2, ok := e.(int)
+//		if !ok {
+//			return false
+//		}
+//		return i == i2
+//	}
+//
+// 2. wrap your value in the implementation
+//
+//	intEq(1).Eq(intEq(2))
+
 // Eq is the equality typeclass: (Eq a) => a -> a -> Bool
 type Eq interface {
 	Eq(Eq) bool
