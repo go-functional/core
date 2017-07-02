983105ca61f2fd1c0f49c0da722f175defdc7126
Aaron Schlesinger <arschles@gmail.com>
2017-07-02 15:52:53 -0700

    using constructor

--- functor/int_example.go
+++ functor/int_example.go
@@ -4,11 +4,11 @@ import (
 	"log"
 )
 
-func intSliceFunctor() {
+func intSliceFunctorExample() {
 	doSomething := func(i int) int {
 		return i + 1
 	}
 	slice := []int{1, 2, 3, 4}
-	results := IntSliceFunctor{ints: slice}.Map(doSomething).Ints()
+	results := ToIntSliceFunctor(slice).Map(doSomething).Ints()
 	log.Println(results)
 }
