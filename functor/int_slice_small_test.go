074c537cadc1963f934b124888376c0f7cb44607
Gabriel Nelle <tehsphinx@web.de>
2019-11-07 19:06:49 +0100

    fix rule description; test on IntSliceFunctor (#15)

--- functor/int_slice_small_test.go
+++ functor/int_slice_small_test.go
@@ -14,3 +14,20 @@ func TestSmallIntSliceFunctor(t *testing.T) {
 	assert.Equal(t, len(mapped.Ints()), len(ints), "resultant ints not the same as original length")
 	assert.Equal(t, mapped.Ints(), ints, "returned int slice")
 }
+
+func TestIntSliceFunctorIntegrity(t *testing.T) {
+	// chaining
+	ints := intSlice(4)
+	functor := LiftIntSlice(ints)
+	m1 := functor.Map(plusOne).Map(timesTwo)
+
+	// function composition
+	ints = intSlice(4)
+	functor = LiftIntSlice(ints)
+	m2 := functor.Map(func(i int) int {
+		return timesTwo(plusOne(i))
+	})
+
+	// compare the results
+	assert.Equal(t, m1.Ints(), m2.Ints(), "returned slices are not equal")
+}
