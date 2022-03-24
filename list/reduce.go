8e53dfcf37b564d21ae337aad4b10b750b92357c
Aaron Schlesinger <aaron@ecomaz.net>
2022-03-24 22:00:27 +0000

    godoc
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- list/reduce.go
+++ list/reduce.go
@@ -1,10 +1,27 @@
 package list
 
-func Reduce[T, U any](
-	slc []T,
-	start U,
-	fn func(U, T) U,
-) U {
+// Reduce reduces slc down to a single element by using fn to build
+// up an accumulator from each value in the list, starting with
+// start.
+//
+// For example, with a list of [1, 2, 3] a start value of "0",
+// and a function that converts each integer in the list to its
+// string representation and appends it to the end of the
+// accumulated string, the execution of this function would look
+// like:
+//
+// 	accumulator := "0", index = 0, element = 1
+//		new accumulator = "0" + "1" = "01"
+// 	accumulator = "01", index = 1, element = 2
+//		new accumulator = "01" + "2" = "012"
+// 	accumulator = "012", index = 2, element = 3
+//		new accumulator = "012" + "3" = "0123"
+//	final result = "0123"
+func Reduce[ValT, AccumT any](
+	slc []ValT,
+	start AccumT,
+	fn func(AccumT, ValT) AccumT,
+) AccumT {
 	ret := start
 	for _, t := range slc {
 		ret = fn(ret, t)
--- values/lazy.go
+++ values/lazy.go
@@ -2,7 +2,12 @@ package values
 
 import "sync"
 
-// Lazy is an interface
+// Lazy is a cached value that will not be computed
+// until it is explictly requested by calling GetLazy.
+// Thereafter, it is cached and returned from the cached value.
+// This construct is best used to hold the result of
+// an expensive operation, the return value of which
+// doesn't change.
 type Lazy[T any] struct {
 	done bool
 	val  T
@@ -11,6 +16,9 @@ type Lazy[T any] struct {
 	f    func() (T, error)
 }
 
+// NewLazy creates a new Lazy value with the given function
+// representing the computation to run to get the value
+// at some point in the future, when it's requested.
 func NewLazy[T any](run func() (T, error)) *Lazy[T] {
 	return &Lazy[T]{
 		done: false,
@@ -19,7 +27,11 @@ func NewLazy[T any](run func() (T, error)) *Lazy[T] {
 	}
 }
 
-func GetLazy[T any](l *Lazy[T]) (T, error) {
+// Get returns the value. If it has already been computed,
+// returns it directly from the cache. Otherwise, runs the
+// (potentially expensive and long running) computation,
+// stores the result in the cache, and returns it.
+func (l *Lazy[T]) Get() (T, error) {
 	l.mut.Lock()
 	defer l.mut.Unlock()
 	if l.done {
@@ -30,11 +42,13 @@ func GetLazy[T any](l *Lazy[T]) (T, error) {
 	return l.val, l.err
 }
 
+// MapLazy returns a new Lazy value that, when requested, will be the
+// result of running f on the return value of l.Get.
 func MapLazy[T, U any](l *Lazy[T], f func(T) (U, error)) *Lazy[U] {
 	l.mut.Lock()
 	defer l.mut.Unlock()
 	return NewLazy(func() (U, error) {
-		t, err := GetLazy(l)
+		t, err := l.Get()
 		var zeroU U
 		if err != nil {
 			return zeroU, err
