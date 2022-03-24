1df7e6f7519ba9bc8f240e86fca207760a66b8c5
Aaron Schlesinger <aaron@ecomaz.net>
2022-03-24 21:47:07 +0000

    godoc
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- list/nonempty.go
+++ list/nonempty.go
@@ -16,15 +16,28 @@ func NewNonempty[T any](head T, tail ...T) Nonempty[T] {
 	}
 }
 
-func (n *Nonempty[T]) toSlice() []T {
+// ToSlice returns n as a slice. Note that every invocation
+// of this function will create a _new_ slice
+func (n *Nonempty[T]) ToSlice() []T {
 	ret := append([]T{n.head}, n.rest...)
 	return ret
 }
 
+// Head returns the head of n.
+// Due to the invariant that the list is non-empty,
+// this function always returns a valid value.
 func (n *Nonempty[T]) Head() T {
 	return n.head
 }
 
+// Tail returns all the elements of n that come after
+// the head. If n has only 1 total elements, this returns
+// nil
 func (n *Nonempty[T]) Tail() []T {
 	return n.rest
+}
+
+// Append appends vals to the end of n
+func (n *Nonempty[T]) Append(vals ...T) {
+	n.rest = append(n.rest, vals...)
 }
